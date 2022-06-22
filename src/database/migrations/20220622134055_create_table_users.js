44b81a74c49649a9b850ee572f0f9af1f487e37c
LucasLevyOB <oliveiralucaslevy@gmail.com>
2022-06-22 10:55:30 -0300

    Criacao dos arquivos index e routes, da pasta database e do arquivo connection, criacao da migration users

---
+++ src/database/migrations/20220622134055_create_table_users.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (table) => {
+    table.increments("usr_id").primary();
+    table.string("usr_name").notNullable();
+    table.string("usr_email").unique().notNullable();
+    table.string("usr_password").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const routes = express.Router();
+
+// const AnsOperatorsController = require("./controllers/AnsOperatorsController");
+
+//user
+
+module.exports = routes;
